9ba13a62e9ecd5381c954ddafa9d283174a56cef
Nournouas <171356831+Nournouas@users.noreply.github.com>
2025-02-19 13:15:45 +0000

    added hover effect

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-
-
 const body = document.querySelector("body");
-body.style.margin = "0"
+body.style.margin = "0";
+body.style.padding = "0";
+body.style.borderWidth = "0";
 body.style.display = "flex";
 body.style.flexDirection = "column";
 body.style.alignItems = "center";
 
-const resetButton = document.createElement("button");
-resetButton.textContent = "reset";
-resetButton.style.marginTop = "10px";
-resetButton.style.width = "150px";
-body.appendChild(resetButton);
+
+function resetEtch(){
+    const newPixels = prompt("How many pixels would you like your new Etch Board to be? (between 1 and 100");
+    if (newPixels === null ){return}
+    
+    const divToRemove = document.querySelector(".containerDiv");
+    if (divToRemove != null) {
+        body.removeChild(divToRemove);
+    }
+    
+
+    if (isNaN(newPixels) || newPixels > 100 || newPixels <= 0){
+        alert("Must be a number between 1 and 700");
+        resetEtch();
+        return
+    }else{
+
+        etchMaker(newPixels);
+    }
+}
 
 
-function etchMaker (pixels){
+function etchMaker(pixels){
     
     const outerContainer = document.createElement("div");
     outerContainer.style.display = "flex";
     outerContainer.style.flexDirection = "column";
     outerContainer.style.width = "90vh";
     outerContainer.style.height = "90vh";
+    outerContainer.classList.add("containerDiv")
 
 
     for (let i = 0; i<pixels ; i++){
@@ -30,9 +46,8 @@ function etchMaker (pixels){
         for (let u = 0; u<pixels ; u++){
             const pixelDiv = document.createElement("div");
             pixelDiv.style.width = 100/pixels + "%";
-            if (u%2 === 0 & i%2 != 0){
-                pixelDiv.style.backgroundColor = "black";
-            }
+            pixelDiv.style.backgroundColor = "black";
+            pixelDiv.addEventListener("mouseenter", e => e.target.style.backgroundColor = "white");
             horizontalContainer.appendChild(pixelDiv);
         }
         outerContainer.appendChild(horizontalContainer);
@@ -41,4 +56,11 @@ function etchMaker (pixels){
     body.appendChild(outerContainer);
 }
 
-etchMaker(1200);
+const resetButton = document.createElement("button");
+resetButton.textContent = "reset";
+resetButton.style.marginTop = "10px";
+resetButton.addEventListener("click", resetEtch);
+resetButton.style.width = "150px";
+body.appendChild(resetButton);
+
+etchMaker(100);
